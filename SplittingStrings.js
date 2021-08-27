3978222f1c0381e41d718e7e849f3a38c9000ce1
Michael Burke <40371938+burke-dev@users.noreply.github.com>
2021-08-27 16:41:46 -0500

    Create SplittingStrings.js

---
+++ SplittingStrings.js
@@ -0,0 +1,24 @@
+function splitValuesByDelimiter(value) {
+  if(typeof value !== 'string') {
+    console.error(`_splitValuesByDelimiter value type must be string\r\nvalue's typeof ${typeof value}\r\n${value.toString()}`);
+    return null;
+  }
+
+  const delimitersUsed = [",", ";", "/", "-"].filter(x => {
+    if(value.includes(x)) {
+      return x;
+    }
+  });
+ 
+  /*
+    If a listed delimiter is entered then this will replace them with a comma then split on each comma returning an array of strings to be used in other functions.
+    This was developed initially for lists of skills or certifications to be listed off on a resume, but since some will want them displayed as bullets while others
+    will have them listed with commas or slashes between them, getting this into a consistent state allows this data to be used easily and consistently
+  */
+  if(delimitersUsed.length) {
+    delimitersUsed.map(x => value = value.replace(new RegExp(x, 'gi'), ","));
+    return value.split(",");
+  }
+  // If no delimiters are used, this simply returns as an array with one entry
+  return [value];
+}
