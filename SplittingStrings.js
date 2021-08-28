290d1689dd1a7ece56472bf706648f522a6f5713
Michael Burke <40371938+burke-dev@users.noreply.github.com>
2021-08-28 15:53:53 -0500

    Merge replace and split
    After consideration, merged with the replace and split by creating a regexp for the split to look for delimiters even with spaces around them to split on them automatically.
    Still have the TODO pending. Also did a refactor to make it a try/catch

--- SplittingStrings.js
+++ SplittingStrings.js
@@ -1,14 +1,16 @@
 function splitValueByMultipleDelimiters(value) {
-  if(typeof value !== 'string') {
-    throw new Error(`splitValueByMultipleDelimiters() value type must be string\r\ntypeof value === ${typeof value}`);
+  try {
+    /*
+      The `value` string entered will be checked for the delimiters `;/-` and replace them with a comma.
+      Then split on each comma returning an array of strings to be used in other functions.
+      This was developed initially for lists of skills or certifications to be listed off on a resume, but since some will want them displayed as bullets while others
+      will have them listed with commas or slashes between them, getting this into a consistent state allows this data to be used easily and consistently.
+      This also filters out empty results for consistency and corrects for human error.
+      TODO - add an escape option to allow delimiters to not be replaced/split on.
+    */
+    return value.split(/\s*[;\-/,]\s*/g).filter(x => x);
   }
-  /*
-    The `value` string entered will be checked for the delimiters `;/-` and replace them with a comma.
-    Then split on each comma returning an array of strings to be used in other functions.
-    This was developed initially for lists of skills or certifications to be listed off on a resume, but since some will want them displayed as bullets while others
-    will have them listed with commas or slashes between them, getting this into a consistent state allows this data to be used easily and consistently.
-    This also filters out empty results for consistency and corrects for human error.
-    TODO - add an escape option to allow delimiters to not be replaced/split on.
-  */
-  return value.replace(/[;/-]/g, ',').split(',').filter(x => x);
+  catch(err) {
+    throw new Error(`splitValueByMultipleDelimiters() value type must be string\r\ntypeof value === ${typeof value}\r\n${err}`);
+  }
 }
